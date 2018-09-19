e8113685741b5dae3830f3b02ab7aeaa9525f443
Justin Doan <ivigideon25@gmail.com>
2018-09-19 15:39:12 -0400

    Added more Objects
    Made seperate widgets their own objects, added in support for bottom informational text.

--- main.py
+++ main.py
@@ -2,44 +2,67 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 
+class InfoText(tk.Label):
 
-class MainApplication(tk.Frame):
-    
     def __init__(self, parent, *args, **kwargs):
-        
+        tk.Label.__init__(self, parent, *args, **kwargs)
+        self.width = 400
+        self.borderwidth=2
+        self.relief = "groove"
+        self.place(relx=1.0, rely=1.0,x=-1,anchor="se")
+        self.config(text="Hello")
+
+class TextArea(tk.Text):
+
+    def __init__(self, parent, *args, **kwargs):
+        tk.Text.__init__(self, parent, *args, **kwargs)
+        self.width=100
+        self.height=30
+        self.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+class MainMenu(tk.Menu):
+
+    def __init__(self, parent, *args,  **kwargs):
+        self.parent = parent
+        tk.Menu.__init__(self, parent, *args, **kwargs)
+        self.file_options = tk.Menu(self, tearoff=0)
+        self.file_options.add_command(label="Open...", command=self.parent.open_file)
+        self.file_options.add_command(label="Save...", command=self.parent.save_file_as)
+        self.add_cascade(label="File", menu=self.file_options)
+        self.add_cascade(label="Clear all", command=lambda: self.parent.textArea.delete(1.0,tk.END))
+class MainApplication(tk.Frame):
+
+    def __init__(self, parent, *args, **kwargs):
+
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
         self.parent.title("pyNotePad")
-        
+
         self.grid(column=0, row=0,sticky=(tk.N,tk.W,tk.E,tk.S))
         self.columnconfigure(0,weight=1)
         self.rowconfigure(0,weight=1)
-        self.text = tk.StringVar()
+
         self.filename = ""
-        self.edit_area = tk.Text(self, width=100, height= 30)
-        self.edit_area.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-        self.main_menu = tk.Menu(root)
-        self.file_options = tk.Menu(self.main_menu, tearoff=0)
-        self.file_options.add_command(label="Open...", command=self.open_file)
-        self.file_options.add_command(label="Save...", command=self.save_file_as)
-        self.main_menu.add_cascade(label="File", menu=self.file_options)
-        self.main_menu.add_cascade(label="Clear all", command=lambda: self.edit_area.delete(1.0,tk.END))
-        self.edit_area.focus()
+
+        self.textArea = TextArea(self)
+        self.main_menu = MainMenu(self)
+        self.InfoText = InfoText(self)
+        self.textArea.focus()
+
         self.parent.bind('<Return>', self.get_text)
         self.parent.bind('<Control-s>', self.save_file)
         self.parent.config(menu=self.main_menu)
 
     def get_text(self,*args):
-            print(self.edit_area.get("1.0", 'end-1c'))
+            print(self.textArea.get("1.0", 'end-1c'))
 
     def open_file(self,*args):
         self.filename = filedialog.askopenfilename(initialdir = "/",title = "Select file", filetypes = (("Text file","*.txt"),("All files","*.*")))
         #we can use our filename to open up our text file
         try:
             with open(self.filename, 'r') as file:
-                self.edit_area.delete('1.0', tk.END)
-                self.edit_area.insert('1.0', file.read())
-            
+                self.textArea.delete('1.0', tk.END)
+                self.textArea.insert('1.0', file.read())
+
         except:
             #Person didn't select a file
             return
@@ -48,22 +71,19 @@ class MainApplication(tk.Frame):
         self.filename = filedialog.asksaveasfile(mode='w', defaultextension=".txt", filetypes = (("Text file","*.txt"),("All files","*.*")))
         if self.filename is None:
             return
-        text = str(self.edit_area.get(1.0, tk.END))
+        text = str(self.textArea.get(1.0, tk.END))
         self.filename.write(text)
         self.filename.close()
 
     def save_file(self,*args):
         with open(self.filename, 'w') as file:
-            text = str(self.edit_area.get(1.0, tk.END))
+            text = str(self.textArea.get(1.0, tk.END))
             file.write(text)
-            file.close()        
-        
+            file.close()
+
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     MainApplication(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
-
-
-
