48dcd15c513a76827e7caca0792b5c609d80ef42
Justin Doan <ivigideon25@gmail.com>
2018-09-21 10:10:31 -0400

    Added info tag
    Added the information label on the bottom right of the program that lets you push a message to the user after an action. Currently it only displays file saved when you save a file via the shortcut

--- main.py
+++ main.py
@@ -3,24 +3,23 @@ from tkinter import ttk
 from tkinter import filedialog
 
 class InfoText(tk.Label):
-
     def __init__(self, parent, *args, **kwargs):
         tk.Label.__init__(self, parent, *args, **kwargs)
         self.width = 400
         self.borderwidth=2
         self.relief = "groove"
         self.place(relx=1.0, rely=1.0,x=-1,anchor="se")
-        self.config(text="Hello")
+        self.config(text="")
 
 class TextArea(tk.Text):
-
     def __init__(self, parent, *args, **kwargs):
         tk.Text.__init__(self, parent, *args, **kwargs)
         self.width=100
         self.height=30
         self.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-class MainMenu(tk.Menu):
 
+
+class MainMenu(tk.Menu):
     def __init__(self, parent, *args,  **kwargs):
         self.parent = parent
         tk.Menu.__init__(self, parent, *args, **kwargs)
@@ -29,8 +28,8 @@ class MainMenu(tk.Menu):
         self.file_options.add_command(label="Save...", command=self.parent.save_file_as)
         self.add_cascade(label="File", menu=self.file_options)
         self.add_cascade(label="Clear all", command=lambda: self.parent.textArea.delete(1.0,tk.END))
-class MainApplication(tk.Frame):
 
+class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
 
         tk.Frame.__init__(self, parent, *args, **kwargs)
@@ -46,11 +45,16 @@ class MainApplication(tk.Frame):
         self.textArea = TextArea(self)
         self.main_menu = MainMenu(self)
         self.InfoText = InfoText(self)
+
         self.textArea.focus()
 
-        self.parent.bind('<Return>', self.get_text)
         self.parent.bind('<Control-s>', self.save_file)
+        self.parent.bind('<Key>', self.clear_info_text)
+
         self.parent.config(menu=self.main_menu)
+
+    def clear_info_text(self, *args):
+        self.InfoText.config(text="")
 
     def get_text(self,*args):
             print(self.textArea.get("1.0", 'end-1c'))
@@ -67,6 +71,7 @@ class MainApplication(tk.Frame):
             #Person didn't select a file
             return
         self.parent.title("pyNotePad - Now Editing " + str(self.filename.split('/')[-1]))
+
     def save_file_as(self,*args):
         self.filename = filedialog.asksaveasfile(mode='w', defaultextension=".txt", filetypes = (("Text file","*.txt"),("All files","*.*")))
         if self.filename is None:
@@ -80,7 +85,7 @@ class MainApplication(tk.Frame):
             text = str(self.textArea.get(1.0, tk.END))
             file.write(text)
             file.close()
-
+            self.InfoText.config(text="File saved")
 
 
 if __name__ == "__main__":
