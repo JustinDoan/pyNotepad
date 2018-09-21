383bbe48c11c24c6ba6ce1ff823e67d5fc373548
Justin Doan <ivigideon25@gmail.com>
2018-09-21 11:16:20 -0400

    Added more placeholder Options

--- main.py
+++ main.py
@@ -24,37 +24,60 @@ class MainMenu(tk.Menu):
         self.parent = parent
         tk.Menu.__init__(self, parent, *args, **kwargs)
         self.file_options = tk.Menu(self, tearoff=0)
-        self.file_options.add_command(label="Open...", command=self.parent.open_file)
-        self.file_options.add_command(label="Save...", command=self.parent.save_file_as)
+        self.file_options.add_command(label='{0: <20}'.format('New'))
+        self.file_options.add_command(label='{0: <20}'.format('Open') + "Ctrl+O", command=self.parent.open_file)
+        self.file_options.add_command(label='{0: <20}'.format('Save')  + "Ctrl+S", command=self.parent.save_file)
+        self.file_options.add_command(label='{0: <20}'.format('Save as...'), command=self.parent.save_file_as)
+        self.file_options.add_command(label='{0: <20}'.format('Exit'), command=self.parent.save_and_quit)
+
+        #Need to be Implemented
+        self.edit_options = tk.Menu(self, tearoff=0)
+        self.edit_options.add_command(label='{0: <20}'.format('Copy') + "Ctrl+C")
+        self.edit_options.add_command(label='{0: <20}'.format('Cut') + "Ctrl+V")
+        self.edit_options.add_command(label='{0: <20}'.format('Select All') + "Ctrl+A")
+
+
+
         self.add_cascade(label="File", menu=self.file_options)
+        self.add_cascade(label="Edit", menu=self.edit_options)
         self.add_cascade(label="Clear all", command=lambda: self.parent.textArea.delete(1.0,tk.END))
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
 
         tk.Frame.__init__(self, parent, *args, **kwargs)
+
+        #Varibles
+        self.filename = ""
+
+
+        #Setting Parent/Parent config
         self.parent = parent
         self.parent.title("pyNotePad")
-
+        #Setting up grid positioning
         self.grid(column=0, row=0,sticky=(tk.N,tk.W,tk.E,tk.S))
         self.columnconfigure(0,weight=1)
         self.rowconfigure(0,weight=1)
 
-        self.filename = ""
 
+        #Creating widgets
         self.textArea = TextArea(self)
         self.main_menu = MainMenu(self)
         self.InfoText = InfoText(self)
-
+        #Setting focus
         self.textArea.focus()
-
+        #Bindings
         self.parent.bind('<Control-s>', self.save_file)
         self.parent.bind('<Key>', self.clear_info_text)
-
+        #Parent Menu configuration
         self.parent.config(menu=self.main_menu)
 
+    #Functions
     def clear_info_text(self, *args):
         self.InfoText.config(text="")
+
+    def set_info_text(self, msg, *args):
+        self.InfoText.config(text=msg)
 
     def get_text(self,*args):
             print(self.textArea.get("1.0", 'end-1c'))
@@ -71,7 +94,8 @@ class MainApplication(tk.Frame):
             #Person didn't select a file
             return
         self.parent.title("pyNotePad - Now Editing " + str(self.filename.split('/')[-1]))
-        self.InfoText.config(text="File opened")
+        self.set_info_text("File Opened")
+
 
     def save_file_as(self,*args):
         self.filename = filedialog.asksaveasfile(mode='w', defaultextension=".txt", filetypes = (("Text file","*.txt"),("All files","*.*")))
@@ -80,16 +104,20 @@ class MainApplication(tk.Frame):
         text = str(self.textArea.get(1.0, tk.END))
         self.filename.write(text)
         self.filename.close()
-        self.InfoText.config(text="File saved")
+        self.set_info_text("File Saved")
 
     def save_file(self,*args):
         with open(self.filename, 'w') as file:
             text = str(self.textArea.get(1.0, tk.END))
             file.write(text)
             file.close()
-            self.InfoText.config(text="File saved")
-
-
+            self.set_info_text("File Saved")
+    def save_and_quit(self, *args):
+        with open(self.filename, 'w') as file:
+            text = str(self.textArea.get(1.0, tk.END))
+            file.write(text)
+            file.close()
+            exit()
 if __name__ == "__main__":
     root = tk.Tk()
     MainApplication(root).pack(side="top", fill="both", expand=True)
